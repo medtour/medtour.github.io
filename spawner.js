376319f1bd887ea11552d5f4345dc816692d7cd3
mdmsb <msb.arkon@gmail.com>
2018-12-26 20:35:36 +0530

    d

--- spawner.js
+++ spawner.js
@@ -37,19 +37,23 @@ document.getElementById("menutoggle").innerHTML =
 "</li>" +
 
 "<li>" +
-"<a  id='about' href='about-us.html'>About-us</a>" +
+"<a  id='about' href='about.html'>About-us</a>" +
 "</li>" +
 
-"<li class='dropdown'>" +
-"<div id='toggle' class='xs-visible' style='float:right;'><img id='arrowtoggle' class='arrowimg1' src='img/arrow.png'></div>" +
-"<a id='event' href='services.html'>Services</a>" +
-"<div id='togglee' class='a dropdown-content'>" +
-"<a href='s1.html'>Service one</a>" +
-"<a href='s2.html'>Service two</a>" +
-"<a href='s3.html'>Service three</a>" +
-"<a href='s4.html'>Service Four</a>" +
-"<a href='#'></a>" +
-"</div>" +
+"<!-- <li class='dropdown'> -->" +
+"<!-- <div id='toggle' class='xs-visible' style='float:right;'><img id='arrowtoggle' class='arrowimg1' src='img/arrow.png'></div> -->" +
+"<!-- <a id='service' href='services.html'>Services</a> -->" +
+"<!-- <div id='togglee' class='a dropdown-content'> -->" +
+"<!-- <a href='s1.html'>Service one</a> -->" +
+"<!-- <a href='s2.html'>Service two</a> -->" +
+"<!-- <a href='s3.html'>Service three</a> -->" +
+"<!-- <a href='s4.html'>Service Four</a> -->" +
+"<!-- <a href='#'></a> -->" +
+"<!-- </div> -->" +
+"<!-- </li> -->" +
+
+"<li>" +
+"<a id='service' href='services.html'>Services</a>" +
 "</li>" +
 
 "<li>" +
@@ -57,7 +61,7 @@ document.getElementById("menutoggle").innerHTML =
 "</li>" +
 
 "<li>" +
-"<a id='contact' href='testimonials.html'>Testimonials</a>" +
+"<a id='testimonial' href='testimonial.html'>Testimonials</a>" +
 "</li>" +
 
 "<li>" +
@@ -74,5 +78,34 @@ document.getElementById("smedia").innerHTML =
 "<a href='#' class='fab fa-youtube'></a>" +
 "<a href='#' class='fab fa-instagram'></a>" ;
 
+
+document.getElementById("footer").innerHTML = 
+
+"<div class='col-sm-4'>" +
+"<h3>LOCATION</h3>" +
+"<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.898636259301!2d77.63539211384365!3d12.978334618244345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae16a30d631f93%3A0x908a5ca987d63664!2sChinmaya+Mission+Hospital+Rd%2C+Indiranagar%2C+Bengaluru%2C+Karnataka!5e0!3m2!1sen!2sin!4v1545304360561' width='90%' height='210px' frameborder='0' style='border:0' allowfullscreen></iframe>" +
+
+"</div>" +
+"<div class='col-sm-4'>" +
+"<h3>CONTACT US</h3>" +
+"<p>Mobile : +91 98765 43211</p>" +
+"<p>Office : +91 98765 43211</p>" +
+"<p>Address : CMH Road, Indranagar,<br> Bangalore-560 000</p>" +
+"<h3>SOCIAL</h3>" +
+"<i class='fab fa-facebook-square'></i>" +
+"<i class='fab fa-twitter'></i>" +
+"<i class='fab fa-youtube'></i>" +
+"<i class='fab fa-instagram'></i>" +
+"</div>" +
+"<div class='col-sm-4'>" +
+"<h3>FOLLOW BLOG VIA EMAIL</h3>" +
+"<div class=''>Enter your email address to follow this blog and receive notifications of new posts by email." +
+
+"<h3>Join 77 other followers</h3>" +
+"<input type='text' name='email' style='width: 95%; padding: 1px 2px' placeholder='Enter your email address' value='' id='subscribe-field'>" +
+"<button class='button'>FOLLOW</button>" +
+"</div>" +
+
+"</div>" ;
 
 }
